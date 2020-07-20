5c112bb3d6e3ec1941690415598b8cbddea59995
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-07-20 19:18:01 -0300

    Removed unnecessary import and configured StatusBar

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes';
 
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { StatusBar } from 'react-native';
 
 import PayButton from './components/PayButton';
 
@@ -30,76 +31,83 @@ const icons = {
 	},
 };
 
-export default function Navigation() {
+export default function Routes() {
 	return (
-		// Container precisa envolver a bottom tab
-		<NavigationContainer>
-			<Tab.Navigator
-				// Desestruturação, dos paramêtros retornados pela função
-				// apenas o route será acessado
-				screenOptions={({ route, navigation }) => ({
-					tabBarIcon: ({ color, size, focused }) => {
-						// Se a tela for Pay, o botão será retornado
-						if (route.name === 'Pay') {
+		<>
+			<StatusBar barStyle="light-content" backgroundColor="#000" />
+			{/* Container precisa envolver a bottom tab */}
+			<NavigationContainer>
+				<Tab.Navigator
+					// Desestruturação, dos paramêtros retornados pela função
+					// apenas o route será acessado
+					screenOptions={({ route, navigation }) => ({
+						tabBarIcon: ({ color, size, focused }) => {
+							// Se a tela for Pay, o botão será retornado
+							if (route.name === 'Pay') {
+								return (
+									<PayButton
+										// Quando o botão é pressionado
+										onPress={() =>
+											navigation.navigate('Pay')
+										}
+										focused={focused}
+									/>
+								);
+							}
+							const { lib: Icon, name } = icons[route.name];
 							return (
-								<PayButton
-									// Quando o botão é pressionado
-									onPress={() => navigation.navigate('Pay')}
-									focused={focused}
-								/>
+								<Icon name={name} size={size} color={color} />
 							);
-						}
-						const { lib: Icon, name } = icons[route.name];
-						return <Icon name={name} size={size} color={color} />;
-					},
-				})}
-				// Customiza as cores da Tab
-				tabBarOptions={{
-					style: {
-						backgroundColor: '#131418',
-						borderTopColor: 'rgba(255, 255, 255, 0.2)',
-					},
-					activeTintColor: '#fff',
-					inactiveTintColor: '#92929c',
-				}}
-			>
-				<Tab.Screen
-					name="Home"
-					component={HomeScreen}
-					options={{
-						title: 'Ínicio',
+						},
+					})}
+					// Customiza as cores da Tab
+					tabBarOptions={{
+						style: {
+							backgroundColor: '#131418',
+							borderTopColor: 'rgba(255, 255, 255, 0.2)',
+						},
+						activeTintColor: '#fff',
+						inactiveTintColor: '#92929c',
 					}}
-				/>
-				<Tab.Screen
-					name="Wallet"
-					component={WalletScreen}
-					options={{
-						title: 'Carteira',
-					}}
-				/>
-				<Tab.Screen
-					name="Pay"
-					component={PayScreen}
-					options={{
-						title: '',
-					}}
-				/>
-				{/* Renderizam a tela pagar pois não serão feitas agora */}
-				<Tab.Screen
-					name="Notifications"
-					component={PayScreen}
-					options={{
-						title: 'Notificações',
-					}}
-				/>
-				<Tab.Screen
-					name="Settings"
-					component={PayScreen}
-					options={{
-						title: 'Ajustes',
-					}}
-				/>
-			</Tab.Navigator>
-		</NavigationContainer>
+				>
+					<Tab.Screen
+						name="Home"
+						component={HomeScreen}
+						options={{
+							title: 'Ínicio',
+						}}
+					/>
+					<Tab.Screen
+						name="Wallet"
+						component={WalletScreen}
+						options={{
+							title: 'Carteira',
+						}}
+					/>
+					<Tab.Screen
+						name="Pay"
+						component={PayScreen}
+						options={{
+							title: '',
+						}}
+					/>
+					{/* Renderizam a tela pagar pois não serão feitas agora */}
+					<Tab.Screen
+						name="Notifications"
+						component={PayScreen}
+						options={{
+							title: 'Notificações',
+						}}
+					/>
+					<Tab.Screen
+						name="Settings"
+						component={PayScreen}
+						options={{
+							title: 'Ajustes',
+						}}
+					/>
+				</Tab.Navigator>
+			</NavigationContainer>
+		</>
 	);
 }
