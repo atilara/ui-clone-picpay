5860620acb280d0dd304178e116d9a59e8e62442
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-07-20 19:18:37 -0300

    Created the top portion of the HomePage, including Header and Suggestions component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import {
+	Wrapper,
+	Header,
+	Container,
+	BalanceContainer,
+	BalanceTitle,
+	Balance,
+} from './styles';
+
+import Suggestions from '../../components/Suggestions';
 
 export default function Home() {
-	return <Text>Home</Text>;
+	return (
+		<Wrapper>
+			<Container>
+				<Header>
+					<MaterialCommunityIcons
+						name="qrcode-scan"
+						size={30}
+						color="#10c86e"
+					/>
+					<BalanceContainer>
+						<BalanceTitle>Meu saldo</BalanceTitle>
+						<Balance>R$ 81,00</Balance>
+					</BalanceContainer>
+					<AntDesign name="gift" size={30} color="#10c86e" />
+				</Header>
+				<Suggestions />
+			</Container>
+		</Wrapper>
+	);
 }
