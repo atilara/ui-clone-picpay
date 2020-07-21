1816fcef1afd143d1f8c5c0a1927d87792e3dd83
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-07-21 13:00:28 -0300

    Corrected labels on Suggestions and removed scroll indicator

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -14,22 +14,22 @@ const items = [
 	{
 		key: String(Math.random()),
 		img: img1,
-		label: 'Doações',
+		label: 'Recarga',
 	},
 	{
 		key: String(Math.random()),
 		img: img2,
-		label: 'Doações',
+		label: 'Uber',
 	},
 	{
 		key: String(Math.random()),
 		img: img3,
-		label: 'Doações',
+		label: 'Ônibus',
 	},
 	{
 		key: String(Math.random()),
 		img: img4,
-		label: 'Doações',
+		label: 'TV',
 	},
 	{
 		key: String(Math.random()),
@@ -39,12 +39,12 @@ const items = [
 	{
 		key: String(Math.random()),
 		img: img6,
-		label: 'Doações',
+		label: 'Barras',
 	},
 	{
 		key: String(Math.random()),
 		img: img7,
-		label: 'Doações',
+		label: 'FAQ',
 	},
 ];
 
--- src/components/Suggestions/styles.js
+++ src/components/Suggestions/styles.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/native';
 // componente dentro de index.js) direto pelo styled components
 export const Container = styled.ScrollView.attrs(() => ({
 	horizontal: true,
+	showsHorizontalScrollIndicator: false,
 	contentContainerStyle: {
 		alignItems: 'center',
 		paddingLeft: 16,
