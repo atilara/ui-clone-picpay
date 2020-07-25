194b51b1251782eb81b604f78802d406a8bc3bf1
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-07-25 16:40:27 -0300

    Added the Services Section to the Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,76 @@
 import React from 'react';
 
+import { Entypo } from '@expo/vector-icons';
+
 import Suggestions from '../../components/Suggestions';
 
-import { Wrapper, Container } from './styles';
+import {
+	Wrapper,
+	Container,
+	Services,
+	Header,
+	Item,
+	Img,
+	Details,
+	Content,
+	Title,
+	Description,
+	Options,
+} from './styles';
+
+import img1 from '../../images/01.png';
+import img6 from '../../images/06.png';
+import img7 from '../../images/07.png';
+
+const services = [
+	{
+		key: String(Math.random()),
+		img: img1,
+		title: 'Recarga de Celular',
+		description: 'Vivo, Claro, Tim, Oi, Nextel, Algar, Serco...',
+	},
+	{
+		key: String(Math.random()),
+		img: img6,
+		title: 'Pagar Conta',
+		description: 'Pague sua conta de luz, água, boletos bancários etc.',
+	},
+	{
+		key: String(Math.random()),
+		img: img7,
+		title: 'Cobrar',
+		description: 'Cobre um amigo',
+	},
+];
 
 export default function Main() {
 	return (
 		<Wrapper>
 			<Container>
 				<Suggestions black />
+				<Services>
+					<Header>Serviços</Header>
+					{services.map((service) => (
+						<Item key={service.key}>
+							<Img source={service.img} />
+							<Details>
+								<Content>
+									<Title>{service.title}</Title>
+									<Description>
+										{service.description}
+									</Description>
+								</Content>
+								<Options>
+									<Entypo
+										name="dots-three-horizontal"
+										size={20}
+										color="rgba(255, 255, 255, 0.6)"
+									/>
+								</Options>
+							</Details>
+						</Item>
+					))}
+				</Services>
 			</Container>
 		</Wrapper>
 	);
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -6,3 +6,46 @@ export const Wrapper = styled.View`
 `;
 
 export const Container = styled.ScrollView``;
+
+export const Services = styled.View``;
+
+export const Header = styled.Text`
+	color: #fff;
+	font-size: 18px;
+	text-transform: uppercase;
+	margin: 0 12px;
+`;
+
+export const Item = styled.TouchableOpacity`
+	flex-direction: row;
+	align-items: center;
+	margin: 12px 0;
+`;
+
+export const Img = styled.Image`
+	margin: 0 12px;
+`;
+
+export const Details = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
+
+export const Content = styled.View`
+	flex: 0.86;
+`;
+
+export const Title = styled.Text`
+	color: #fff;
+	font-size: 18px;
+	font-weight: bold;
+	margin-bottom: 4px;
+`;
+
+export const Description = styled.Text`
+	color: #fff;
+`;
+
+export const Options = styled.TouchableOpacity`
+	margin: 0 12px;
+`;
