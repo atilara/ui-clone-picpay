c4a2b0b21f38cddb2d31f69acee794432483f634
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-03-16 23:24:21 -0300

    style: correct identation of routes.js

--- src/routes.js
+++ src/routes.js
@@ -15,100 +15,96 @@ import SettingsScreen from './pages/Settings';
 const Tab = createBottomTabNavigator();
 
 const icons = {
-	Home: {
-		lib: AntDesign,
-		name: 'home',
-	},
-	Wallet: {
-		lib: AntDesign,
-		name: 'creditcard',
-	},
-	Notifications: {
-		lib: Ionicons,
-		name: 'ios-notifications-outline',
-	},
-	Settings: {
-		lib: AntDesign,
-		name: 'setting',
-	},
+  Home: {
+    lib: AntDesign,
+    name: 'home',
+  },
+  Wallet: {
+    lib: AntDesign,
+    name: 'creditcard',
+  },
+  Notifications: {
+    lib: Ionicons,
+    name: 'ios-notifications-outline',
+  },
+  Settings: {
+    lib: AntDesign,
+    name: 'setting',
+  },
 };
 
 export default function Routes() {
-	return (
-		<>
-			<StatusBar barStyle="light-content" backgroundColor="#000" />
-			{/* Container precisa envolver a bottom tab */}
-			<NavigationContainer>
-				<Tab.Navigator
-					// Desestruturação, dos paramêtros retornados pela função
-					// apenas o route será acessado
-					screenOptions={({ route, navigation }) => ({
-						tabBarIcon: ({ color, size, focused }) => {
-							// Se a tela for Pay, o botão será retornado
-							if (route.name === 'Pay') {
-								return (
-									<PayButton
-										// Quando o botão é pressionado
-										onPress={() =>
-											navigation.navigate('Pay')
-										}
-										focused={focused}
-									/>
-								);
-							}
-							const { lib: Icon, name } = icons[route.name];
-							return (
-								<Icon name={name} size={size} color={color} />
-							);
-						},
-					})}
-					// Customiza as cores da Tab
-					tabBarOptions={{
-						style: {
-							backgroundColor: '#131418',
-							borderTopColor: 'rgba(255, 255, 255, 0.2)',
-						},
-						activeTintColor: '#fff',
-						inactiveTintColor: '#92929c',
-					}}
-				>
-					<Tab.Screen
-						name="Home"
-						component={HomeScreen}
-						options={{
-							title: 'Ínicio',
-						}}
-					/>
-					<Tab.Screen
-						name="Wallet"
-						component={WalletScreen}
-						options={{
-							title: 'Carteira',
-						}}
-					/>
-					<Tab.Screen
-						name="Pay"
-						component={PayScreen}
-						options={{
-							title: '',
-						}}
-					/>
-					<Tab.Screen
-						name="Notifications"
-						component={NotificationsScreen}
-						options={{
-							title: 'Notificações',
-						}}
-					/>
-					<Tab.Screen
-						name="Settings"
-						component={SettingsScreen}
-						options={{
-							title: 'Ajustes',
-						}}
-					/>
-				</Tab.Navigator>
-			</NavigationContainer>
-		</>
-	);
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      {/* Container precisa envolver a bottom tab */}
+      <NavigationContainer>
+        <Tab.Navigator
+          // Desestruturação, dos paramêtros retornados pela função
+          // apenas o route será acessado
+          screenOptions={({ route, navigation }) => ({
+            tabBarIcon: ({ color, size, focused }) => {
+              // Se a tela for Pay, o botão será retornado
+              if (route.name === 'Pay') {
+                return (
+                  <PayButton
+                    // Quando o botão é pressionado
+                    onPress={() => navigation.navigate('Pay')}
+                    focused={focused}
+                  />
+                );
+              }
+              const { lib: Icon, name } = icons[route.name];
+              return <Icon name={name} size={size} color={color} />;
+            },
+          })}
+          // Customiza as cores da Tab
+          tabBarOptions={{
+            style: {
+              backgroundColor: '#131418',
+              borderTopColor: 'rgba(255, 255, 255, 0.2)',
+            },
+            activeTintColor: '#fff',
+            inactiveTintColor: '#92929c',
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              title: 'Ínicio',
+            }}
+          />
+          <Tab.Screen
+            name="Wallet"
+            component={WalletScreen}
+            options={{
+              title: 'Carteira',
+            }}
+          />
+          <Tab.Screen
+            name="Pay"
+            component={PayScreen}
+            options={{
+              title: '',
+            }}
+          />
+          <Tab.Screen
+            name="Notifications"
+            component={NotificationsScreen}
+            options={{
+              title: 'Notificações',
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+              title: 'Ajustes',
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
